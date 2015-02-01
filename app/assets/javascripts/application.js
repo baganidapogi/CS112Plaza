cd3837ed3136e974c093ce7d0c6db425c9fd6fa3
bagani sularte <baganisularte@gmail.com>
2015-02-01 22:55:47 +0800

    sqlite3 fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,11 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-<<<<<<< HEAD
+
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-=======
+
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
->>>>>>> 929a8b7df4b79efeb2e27943eeb0b5bceebb9a04
+
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
